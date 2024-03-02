65fe201e8fcf0ae22faa84dd037b59a39b05c98c
Vaderiana <120415111+Vaderiana@users.noreply.github.com>
2024-03-02 12:56:43 +0300

    Delivery Card test

---
+++ src/test/java/TestDeliveryCard.java
@@ -0,0 +1,34 @@
+import com.codeborne.selenide.Condition;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestDeliveryCard {
+
+    @Test
+    public void positiveTest() {
+        open("http://localhost:7777");
+        $(By.cssSelector("[placeholder='Город']")).sendKeys("Москва");
+        $(By.cssSelector("[name='name']")).sendKeys("Ермолина Валерия");
+        $(By.cssSelector("[name='phone']")).sendKeys("+79666666666");
+        $(By.cssSelector("[class='checkbox__text']")).click();
+        $(By.cssSelector("button.button")).click();
+        $("[class='notification__title']").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        String text = $("[class='notification__title']").getText();
+        assertEquals("Успешно!", text);
+    }
+}
+
